860282f50c37b04d47aef32a7f03f3ddd5959500
bedi0021 <bedi0021@algonquinlive.com>
2020-07-27 21:25:18 -0400

    Front Page Done

--- app/src/main/java/com/example/finalproject20s/MainActivity.java
+++ app/src/main/java/com/example/finalproject20s/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.finalproject20s;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button GDSbutton, SMHbutton, SLSbutton, DSSbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.front_button_page);
+
+        GDSbutton=(Button)findViewById(R.id.GDSbtn);
+        SMHbutton=(Button)findViewById(R.id.SMHbtn);
+        SLSbutton=(Button)findViewById(R.id.SLSbtn);
+        DSSbutton=(Button)findViewById(R.id.DSSbtn);
+
+        GDSbutton.setOnClickListener(btn -> {
+            //Geo Data sourse
+            //Add your respective Activity
+        });
+
+        SMHbutton.setOnClickListener(btn -> {
+            //Soccer Match Highlights
+            //Add your respective Activity
+        });
+
+        SLSbutton.setOnClickListener(btn -> {
+            //Song Lyrics Search
+            //Add your respective Activity
+        });
+
+        DSSbutton.setOnClickListener(btn -> {
+            //Deezer Song Search
+            //Add your respective Activity
+        });
+
+
     }
 }
